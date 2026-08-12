26a12617a2871f58a6482845d5f58ce9584f721d
Niki Koenemann <niktoseson@gmail.com>
2026-08-12 10:20:22 +0200

    Add JavaScript math exercises

---
+++ niki/math.js
@@ -0,0 +1,12 @@
+let radius = 5;
+let flaeche = Math.PI * radius ** 2;
+
+console.log(flaeche);
+
+let zahl = 17;
+
+if (zahl % 2 === 0) {
+    console.log("Die Zahl ist gerade");
+} else {
+    console.log("Die Zahl ist ungerade");
+}
