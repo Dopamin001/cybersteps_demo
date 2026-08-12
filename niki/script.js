85af2c7762fd7e2a4e37deb71d8a3e62df90baaf
Niki Koenemann <niktoseson@gmail.com>
2026-08-12 09:49:25 +0200

    Add JavaScript data types

---
+++ niki/script.js
@@ -0,0 +1,9 @@
+let zahl = 42;
+let text = "Cybersecurity";
+let bool = true;
+let ports = [22, 80, 443];
+
+console.log(typeof zahl);
+console.log(typeof text);
+console.log(typeof bool);
+console.log(typeof ports);
