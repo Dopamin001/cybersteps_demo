088b0f3a7ac6c40e7c0fd02255bcb37137ae0f2b
Tom Thomas <tom@thomas-meiningen.de>
2026-08-12 09:50:19 +0200

    Tom: Variablen und typeof

---
+++ Tom/variablen.js
@@ -0,0 +1,9 @@
+const zahl = 42;
+const text = "Hallo Cybersteps";
+const istAktiv = true;
+const liste = [1, 2, 3];
+
+console.log(typeof zahl);      // "number"
+console.log(typeof text);      // "string"
+console.log(typeof istAktiv);  // "boolean"
+console.log(typeof liste);     // "object"  ← nicht "array"!
